b74fc2998ea1ab5469784f433e3a92debc932502
alexcnsf <alexcnasoni@gmail.com>
2024-09-18 12:44:57 -0700

    readme formatting

---
+++ ragnews/evaluate.py
@@ -0,0 +1,5 @@
+'''
+This file is to evaluate the quality of our RAG system using the Hairy Trumpt tool/dataset
+'''
+
+import ragnews
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ragnews",
+    version="0.1",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],  # Add any dependencies here if needed
+)
+
